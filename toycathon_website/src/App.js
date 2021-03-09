19d7925b93c3c6e4c7e32deac5880d853f2ccc3d
Mehul Saxena <2019kuec2019@iiitkota.ac.in>
2021-03-10 01:45:24 +0530

    Code Updated to React

--- toycathon_website/src/App.js
+++ toycathon_website/src/App.js
@@ -4,7 +4,13 @@ import Home from './Home.js';
 import About from './About.js'
 import Features from './Features.js'
 import Header from './Header.js'
+import CTA from './CTA.js'
 import "./style.css";
+import Footer from './Footer.js';
+import Faq from './Faq.js';
+import Services from './Services'
+import Pricing from './Pricing.js';
+import Testimonial from './Testimonial.js';
 
 
 function App() {
@@ -15,7 +21,15 @@ function App() {
       <main id="main">
         <About></About>
         <Features></Features>
-      </main>
+        <CTA></CTA>
+        <Services></Services>
+        <Testimonial></Testimonial>
+        <Pricing></Pricing>
+        <Faq></Faq>
+        </main>
+
+      <Footer></Footer>
+      
     </div>
 
   );
---
+++ toycathon_website/src/CTA.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './style.css';
+
+function CTA(){
+    return(
+        <section id="cta" class="cta">
+      <div class="container">
+
+        <div class="row" data-aos="zoom-out">
+          <div class="col-lg-9 text-center text-lg-left">
+            <h3>Extra</h3>
+            <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-lg-3 cta-btn-container text-center">
+            <a class="cta-btn align-middle" href="#">Call To Action</a>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    )
+}
+
+export default CTA;

991f9ddf044102fcb88b7404d4e2925c895bc55a
Akshat Saxena <2019kucp1067@iiitkota.ac.in>
2021-03-25 12:38:24 +0530

    probelm statements added

--- toycathon_website/src/screens/Services.js
+++ toycathon_website/src/screens/Services.js
@@ -11,51 +11,60 @@ return(
         </div>
 
         <div class="row">
-          <div class="col-lg-4 col-md-6">
+          <div class="col-lg-5 col-md-6">
             <div class="icon-box" data-aos="zoom-in-left">
               <div class="icon"><i class="las la-basketball-ball" ></i></div>
-              <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+              <h4 class="title"><a href="">Theme : Health and Creative Thinking</a></h4>
+              <p class="description">To develop a Multiplayer Gaming Application/Animation that focuses on enhancing Emotional Intelligence and fight depression</p>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-5 mt-md-0">
-            <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="100">
-              <div class="icon"><i class="las la-book" ></i></div>
-              <h4 class="title"><a href="">Dolor Sitema</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+          <div class="col-lg-7 col-md-6" data-toggle="collapse"> 
+            <div class="icon-box" data-aos="zoom-in-left">
+              <h4 class="title"><a href="">Overview</a></h4>
+              <p class="description">Adolescents and people from other age groups have been experiencing deteriorating mental health owing to loneliness, lack of friends, inability to meet people due to very busy lifestyles and stress. This has resulted in many mental health and emotional disorders including serious depression. Many youngsters commit suicide as they cannot handle depression. Therefore, the challenge is to develop a game or digital application that can help fight depression and alerts an individual if she/he is undergoing any major symptoms of mental health disorder.</p>
             </div>
           </div>
-
-          <div class="col-lg-4 col-md-6 mt-5 mt-lg-0 ">
-            <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="200">
-              <div class="icon"><i class="las la-file-alt" ></i></div>
-              <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+          <div class="col-lg-5 col-md-6">
+            <div class="icon-box" data-aos="zoom-in-left">
+              <div class="icon"><i class="las la-basketball-ball" ></i></div>
+              <h4 class="title"><a href="">Theme : Academics and Enhanced Learning</a></h4>
+              <p class="description">Helping children learn Letter Sound association for Early Literacy</p>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-5">
-            <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="300">
-              <div class="icon"><i class="las la-tachometer-alt" ></i></div>
-              <h4 class="title"><a href="">Magni Dolores</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+          <div class="col-lg-7 col-md-6">
+            <div class="icon-box" data-aos="zoom-in-left">
+              <h4 class="title"><a href="">Overview</a></h4>
+              <p class="description">Young Children struggle to remember letter sounds and thus face problems in reading. Children are expected to associate letter/s with sounds. For instance, in the word “Purse” the initial sound is “P”. Inability to be able to associate the word with sound leads to problems in reading among children and it helps to make links between the unfamiliar print words to the spoken language. The objective here is to enhance children’s early learning so that they can easily learn and associate letter/sound association and get prepared for early literacy.</p>
             </div>
           </div>
-
-          <div class="col-lg-4 col-md-6 mt-5">
-            <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="400">
-              <div class="icon"><i class="las la-globe-americas" ></i></div>
-              <h4 class="title"><a href="">Nemo Enim</a></h4>
-              <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+          <div class="col-lg-5 col-md-6">
+            <div class="icon-box" data-aos="zoom-in-left">
+              <div class="icon"><i class="las la-basketball-ball" ></i></div>
+              <h4 class="title"><a href="">Theme : Able for Disables</a></h4>
+              <p class="description">Design a Dynamic moving toy or animation for teenagers with hearing impairment and wanting to learn about musical notes and beats.</p>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6 mt-5">
-            <div class="icon-box" data-aos="zoom-in-left" data-aos-delay="500">
-              <div class="icon"><i class="las la-clock" ></i></div>
-              <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-              <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+          <div class="col-lg-7 col-md-6">
+            <div class="icon-box" data-aos="zoom-in-left">
+              <h4 class="title"><a href="">Overview</a></h4>
+              <p class="description">A moving/vibrating toy may be designed to convert the auditory signal of musical notes and patterns,visuals so as to make people with hearing impairments be able to enjoy and understand music through a different sensory experience</p>
+            </div>
+          </div>
+          <div class="col-lg-5 col-md-6">
+            <div class="icon-box" data-aos="zoom-in-left">
+              <div class="icon"><i class="las la-basketball-ball" ></i></div>
+              <h4 class="title"><a href="">Theme : Indian Culture and History</a></h4>
+              <p class="description">Design toys/games/animation highlighting contribution of Indian sages towards Science, Maths and Literature.</p>
+            </div>
+          </div>
+          <div class="col-lg-7 col-md-6">
+            <div class="icon-box" data-aos="zoom-in-left">
+              <h4 class="title"><a href="">Overview</a></h4>
+              <p class="description">Famous Indian Sages like Acharya Kanad, Acharya Patanjali and many more have contributed enormously for the development of Science, Maths and Literature but the younger generations are not aware of their work.Games/Toys/Simulations can be developed highlighting their work and emphasizing India’s rich academic traditions.</p>
             </div>
           </div>
         </div>
+          
 
       </div>
     </section>

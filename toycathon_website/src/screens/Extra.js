970224fafe4267153d905bfb7d1a1b12c5ea46d6
Sanatan Shrivastava <sanatan.cseiiitk@gmail.com>
2021-04-11 13:13:59 +0530

    changed animate description

--- toycathon_website/src/screens/Extra.js
+++ toycathon_website/src/screens/Extra.js
@@ -1,24 +1,41 @@
 import React from 'react';
 
 function Extra(){
-    return(
-        <section id="cta" class="cta">
-      <div class="container">
-        <hr />
+    return (
+      <section id="cta" class="cta">
+        <div class="container">
+          <hr />
 
-        <div class="row" data-aos="zoom-out">
-          <div class="col-lg-9 text-center text-lg-left">
-            <h3>What is Animate?</h3>
-            <p>Animate is a 48-hour all-accessible hackathon centered on current issues from the field of toy and animation about finding ways to enhance your lifestyle by correlating it to lost heritage and culture of India. It will provide opportunities for students to innovate and build a project. Emphasis will be placed on alleviating social issues through research and collaboration. Through .hack, students will be exposed to a hands-on innovation opportunity that drives social impact through technology, leads them to expand their knowledge of current issues, and to pursue greater career aspirations.</p>
+          <div class="row" data-aos="zoom-out">
+            <div class="col-lg-9 text-center text-lg-left">
+              <h3>What is Animate?</h3>
+              <p>
+                Animate is a 48-hour all-accessible hackathon centered on
+                current issues from the field of toy and animation about finding
+                and improvising the ideas in a more wholistic and beneficial
+                way. It will provide opportunities for students to innovate and
+                build a project. Emphasis will be placed on alleviating social
+                issues through research and collaboration. Through Animate Hackathon,
+                students will be exposed to a hands-on innovation opportunity
+                that drives social impact through technology, leads them to
+                expand their knowledge of current issues, and to pursue greater
+                career aspirations.
+              </p>
+            </div>
+            <div class="col-lg-3 cta-btn-container text-center">
+              <a
+                class="cta-btn align-middle"
+                href="https://skillenza.com/challenge/animate-dsciiitkota"
+                target="_blank"
+              >
+                Register
+              </a>
+            </div>
           </div>
-          <div class="col-lg-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="https://skillenza.com/challenge/animate-dsciiitkota" target="_blank">Register</a>
-          </div>
+          <hr className="second" />
         </div>
-        <hr className="second"/>
-      </div>
-    </section>
-    )
+      </section>
+    );
 }
 
 export default Extra;

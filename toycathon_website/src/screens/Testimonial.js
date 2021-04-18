e493d6c8c02cbaf48d283b298f1163a7db4706c0
mradul71 <mradulagarwal790@gmail.com>
2021-04-18 10:42:21 +0530

    final

--- toycathon_website/src/screens/Testimonial.js
+++ toycathon_website/src/screens/Testimonial.js
@@ -9,17 +9,16 @@ function Testimonial(){
             <h2>Speakers</h2>
             <p>Our Guests</p>
         </div>
-          <h5 class="titles" style={{padding: "0px 10px"}}> Stay Tuned ...</h5>
 
-          {/* <div class="row speakers">
-            <div class="card speak_card" data-aos="zoom-in" >
-              <img class="card-img-top" src="assets/img/codechef.jpg" alt="Card image cap"></img>
+          <div class="row speakers">
+            <div class="card speak_card">
+              <img class="card-img-top" src="assets/img/speaker1.jpg" alt="Card image cap"></img>
               <div class="card-body">
-                <p class="card-text"></p>
+                <p class="card-text">Sanath Murali</p>
               </div>
             </div>
 
-            <div class="card speak_card" data-aos="zoom-in" data-aos-delay="200" >
+            {/* <div class="card speak_card" data-aos="zoom-in" data-aos-delay="200" >
               <img class="card-img-top" src="assets/img/testimonials/testimonials-2.jpg" alt="Card image cap"></img>
               <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -31,8 +30,8 @@ function Testimonial(){
               <div class="card-body">
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
               </div>
-            </div>
-          </div> */}
+            </div>*/}
+          </div> 
           
   
         </div>
